c67c7f0a80b8aff90794573e05dbfdf8839337f7
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-10 01:16:08 +0300

    MM

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -44,7 +44,8 @@ def before_request():
     """ befor request """
     if auth is not None:
         if auth.require_auth(request.path, [
-            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+                '/api/v1/status/', '/api/v1/unauthorized/',
+                '/api/v1/forbidden/']):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
