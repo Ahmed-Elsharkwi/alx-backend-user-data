08e243f282a7f18a635bab881b91ba7983ec7c6b
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-10 01:12:45 +0300

    MM

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ authorization header """
-        return None
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user """
