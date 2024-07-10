1c3df4e457a39a1f2f72ad62274dbf9dd50a9693
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-11 00:09:01 +0300

    MM

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -55,7 +55,9 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """ return user instance """
-        if user_email is None or type(user_email) is not str:
+        if user_email is None or type(
+                user_email) is not str or user_pwd is None or type(
+                        user_pwd) is not str:
             return None
         new_list = User.search({'email': user_email})
         if len(new_list) <= 0:
