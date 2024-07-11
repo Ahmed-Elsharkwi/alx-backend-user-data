9bdf2864e34f8d38d6d29baa81ac3a8d575b9f9b
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-12 00:48:33 +0300

    MM

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,8 +54,10 @@ def before_request():
     if auth is not None:
         if auth.require_auth(request.path, [
                 '/api/v1/status/', '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']):
-            if auth.authorization_header(request) is None:
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/']):
+            if auth.authorization_header(
+                    request) is None or auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
