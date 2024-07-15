2bef882bfa78308c46814d0c8c54adf15690b69e
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-15 12:39:24 +0300

    MM

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" user module """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, VARCHAR
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250), nullable=True)
+    reset_token = Column(VARCHAR(250), nullable=True)
