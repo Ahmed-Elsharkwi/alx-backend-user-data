23ece2489a31608b56275818a348cd36f150d0f3
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-18 00:46:16 +0300

    MM

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+app module
+"""
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False, methods=['GET'])
+def reload():
+    """ reload something """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
