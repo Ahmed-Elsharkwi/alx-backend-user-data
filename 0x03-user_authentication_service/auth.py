94f3be96e00d0b2c552997c4184425f9517faf5b
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-18 00:01:49 +0300

    MM

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,29 @@
 """
 auth module
 """
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register new user """
+        password = _hash_password(password)
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound or InvalidRequestError:
+            user = self._db.add_user(email, password)
+        return user
 
 
 def _hash_password(password: str) -> bytes:
