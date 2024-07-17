671761a90c98af668509fea80a31dc4d5818bb36
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-18 00:19:10 +0300

    MM

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,7 +21,7 @@ class Auth:
         try:
             self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exists')
-        except NoResultFound or InvalidRequestError:
+        except NoResultFound:
             password = _hash_password(password)
             user = self._db.add_user(email, password)
             return user
