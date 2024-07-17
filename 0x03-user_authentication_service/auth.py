2fa53090fe96b2f2e741d252f3bee68ea0fae993
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-17 22:24:38 +0300

    MM

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,9 +4,18 @@ auth module
 """
 import bcrypt
 
-
-def _hash_password(password):
-    """ hashing the password """
+"""
+def _hash_password(password: str) -> bytes:
+    #hashing the password
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    return (bcrypt.hashpw(bytes, salt))
+    return bcrypt.hashpw(bytes, salt)
+"""
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password string and returns it in bytes form
+    Args:
+        password (str): password in string format
+    """
+    passwd = password.encode('utf-8')
+    return bcrypt.hashpw(passwd, bcrypt.gensalt())
