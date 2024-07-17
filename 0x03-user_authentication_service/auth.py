1256685712dfac99649a3aca941ba26435068e90
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-17 22:11:00 +0300

    MM

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,5 +9,4 @@ def _hash_password(password):
     """ hashing the password """
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    print(salt)
     return (bcrypt.hashpw(bytes, salt))
